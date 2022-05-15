0e7311bf47d95bebaf42aeeffc46e32fe37c6144
thounny <thounnykeo@gmail.com>
2022-05-14 20:40:14 -0400

    Implemented custom cursor JS

--- static/main.js
+++ static/main.js
@@ -86,3 +86,35 @@ TweenMax.from(".right-bottom-text", 2, {
     opacity: 0,
     ease: Expo.easeInOut
 });
+
+// cursor
+const cursor = document.querySelector('.cursor');
+
+let mouseX = 0;
+let mouseY = 0;
+
+let cursorX = 0;
+let cursorY = 0;
+
+let speed = 0.5; // change to increase the ease
+
+function animate() {
+    let distX = mouseX - cursorX;
+    let distY = mouseY - cursorY;
+
+    cursorX = cursorX + (distX * speed);
+    cursorY = cursorY + (distY * speed);
+
+    cursor.style.left = cursorX + 'px';
+    cursor.style.top = cursorY + 'px';
+
+    requestAnimationFrame(animate);
+}
+
+
+animate();
+
+document.addEventListener('mousemove', (event) => {
+    mouseX = event.pageX;
+    mouseY = event.pageY;
+})
